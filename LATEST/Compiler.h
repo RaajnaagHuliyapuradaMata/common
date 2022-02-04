efdeb3fb6435742251ae1db4155703a3ed5976a0
root <nagaraja.hm.0001@gmail.com>
2022-02-04 10:23:48 +0100

    Update: submodule common

--- LATEST/Compiler.h
+++ LATEST/Compiler.h
@@ -1,23 +1,59 @@
 #pragma once
+/*****************************************************/
+/* File   : Compiler.h                               */
+/* Author : Naagraaj HM                              */
+/*****************************************************/
 
+/*****************************************************/
+/* #INCLUDES                                         */
+/*****************************************************/
 #include "Compiler_Cfg.h"
 
+/*****************************************************/
+/* #DEFINES                                          */
+/*****************************************************/
 #define AUTOMATIC
 #define TYPEDEF
 #define NULL_PTR ((void *)0)
 #define INLINE inline
 #define LOCAL_INLINE static inline
+
+/*****************************************************/
+/* MACROS                                            */
+/*****************************************************/
 #define VAR(type, memclass) memclass type
 #define CONST(type, memclass) memclass const type
 #define FUNC(rettype, memclass) memclass rettype
 #define P2VAR(type, memclass, ptrclass) memclass ptrclass type *
 #define P2CONST(type, memclass, ptrclass) memclass const ptrclass type *
 #define P2FUNC(rettype, ptrlass, fctname) ptrclass rettype (* fctname)
-
 #define FUNC_P2CONST(rettype, ptrclass, memclass) const ptrclass rettype * memclass
 #define FUNC_P2VAR(rettype, ptrclass, memclass) ptrclass rettype * memclass
 #define CONSTP2VAR(ptrtype, memclass, ptrclass) ptrclass ptrtype * const memclass
 #define CONSTP2CONST(ptrtype, memclass, ptrclass) const ptrclass ptrtype * const memclass
 #define CONSTP2FUNC(rettype, ptrlass, fctname) ptrclass rettype (* const fctname)
 
+/*****************************************************/
+/* TYPEDEFS                                          */
+/*****************************************************/
+
+/*****************************************************/
+/* CONSTS                                            */
+/*****************************************************/
+
+/*****************************************************/
+/* PARAMS                                            */
+/*****************************************************/
+
+/*****************************************************/
+/* OBJECTS                                           */
+/*****************************************************/
+
+/*****************************************************/
+/* FUNCTIONS                                         */
+/*****************************************************/
+
+/*****************************************************/
+/* EOF                                               */
+/*****************************************************/
 
