8f57b0b5c4c90446d112714acce89802cbf3fe11
root <nagaraja.hm.0001@gmail.com>
2022-03-14 13:03:47 +0100

    Update: AUTOSAR basic software module common

--- LATEST/Compiler.h
+++ LATEST/Compiler.h
@@ -1,59 +1,59 @@
 #pragma once
-/*****************************************************/
-/* File   : Compiler.h                               */
-/* Author : Naagraaj HM                              */
-/*****************************************************/
+/******************************************************************************/
+/* File   : Compiler.h                                                        */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
 
-/*****************************************************/
-/* #INCLUDES                                         */
-/*****************************************************/
+/******************************************************************************/
+/* #INCLUDES                                                                  */
+/******************************************************************************/
 #include "Compiler_Cfg.h"
 
-/*****************************************************/
-/* #DEFINES                                          */
-/*****************************************************/
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
 #define AUTOMATIC
 #define TYPEDEF
-#define NULL_PTR ((void *)0)
-#define INLINE inline
-#define LOCAL_INLINE static inline
+#define NULL_PTR                                                     ((void *)0)
+#define INLINE                                                            inline
+#define LOCAL_INLINE                                               static inline
 
-/*****************************************************/
-/* MACROS                                            */
-/*****************************************************/
-#define VAR(type, memclass) memclass type
-#define CONST(type, memclass) memclass const type
-#define FUNC(rettype, memclass) memclass rettype
-#define P2VAR(type, memclass, ptrclass) memclass ptrclass type *
-#define P2CONST(type, memclass, ptrclass) memclass const ptrclass type *
-#define P2FUNC(rettype, ptrlass, fctname) ptrclass rettype (* fctname)
+/******************************************************************************/
+/* MACROS                                                                     */
+/******************************************************************************/
+#define VAR(type, memclass)                       memclass type
+#define CONST(type, memclass)                     memclass const type
+#define FUNC(rettype, memclass)                   memclass rettype
+#define P2VAR(type, memclass, ptrclass)           memclass ptrclass type*
+#define P2CONST(type, memclass, ptrclass)         memclass const ptrclass type*
+#define P2FUNC(rettype, ptrlass, fctname)         ptrclass rettype (* fctname)
 #define FUNC_P2CONST(rettype, ptrclass, memclass) const ptrclass rettype * memclass
-#define FUNC_P2VAR(rettype, ptrclass, memclass) ptrclass rettype * memclass
-#define CONSTP2VAR(ptrtype, memclass, ptrclass) ptrclass ptrtype * const memclass
-#define CONSTP2CONST(ptrtype, memclass, ptrclass) const ptrclass ptrtype * const memclass
-#define CONSTP2FUNC(rettype, ptrlass, fctname) ptrclass rettype (* const fctname)
+#define FUNC_P2VAR(rettype, ptrclass, memclass)   ptrclass rettype * memclass
+#define CONSTP2VAR(ptrtype, memclass, ptrclass)   ptrclass ptrtype* const memclass
+#define CONSTP2CONST(ptrtype, memclass, ptrclass) const ptrclass ptrtype* const memclass
+#define CONSTP2FUNC(rettype, ptrlass, fctname)    ptrclass rettype (* const fctname)
 
-/*****************************************************/
-/* TYPEDEFS                                          */
-/*****************************************************/
+/******************************************************************************/
+/* TYPEDEFS                                                                   */
+/******************************************************************************/
 
-/*****************************************************/
-/* CONSTS                                            */
-/*****************************************************/
+/******************************************************************************/
+/* CONSTS                                                                     */
+/******************************************************************************/
 
-/*****************************************************/
-/* PARAMS                                            */
-/*****************************************************/
+/******************************************************************************/
+/* PARAMS                                                                     */
+/******************************************************************************/
 
-/*****************************************************/
-/* OBJECTS                                           */
-/*****************************************************/
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
 
-/*****************************************************/
-/* FUNCTIONS                                         */
-/*****************************************************/
+/******************************************************************************/
+/* FUNCTIONS                                                                  */
+/******************************************************************************/
 
-/*****************************************************/
-/* EOF                                               */
-/*****************************************************/
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
 
