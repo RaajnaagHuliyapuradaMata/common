288e097032814808888839bee4e17c0d547b3203
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-07-14 19:06:59 +0000

    Initial commit

---
+++ LATEST/b_git_pull.sh
@@ -0,0 +1,257 @@
+echo -e "\n\nPlatform:"
+cd ../../..
+git pull
+
+
+echo -e "\n\nproject:"
+cd projects/Prj_Samsung_VirtualECU_AndroidAMD_Ubuntu20x/LATEST
+git pull
+
+echo -e "\n\nSmbLnk2_Cfg_Gen:"
+cd source/SmbLnk2_CfgGen
+git pull
+
+echo -e "\n\nSmbLnk2_CanIf:"
+cd ../SmbLnk2_CanIf
+git pull
+
+echo -e "\n\nSmbLnk2_CanNm:"
+cd ../SmbLnk2_CanNm
+git pull
+
+echo -e "\n\nSmbLnk2_CanSm:"
+cd ../SmbLnk2_CanSm
+git pull
+
+echo -e "\n\nSmbLnk2_CanTp:"
+cd ../SmbLnk2_CanTp
+git pull
+
+echo -e "\n\nSmbLnk2_CryIf:"
+cd ../SmbLnk2_CryIf
+git pull
+
+echo -e "\n\nSmbLnk2_Ea:"
+cd ../SmbLnk2_Ea
+git pull
+
+echo -e "\n\nSmbLnk2_EthIf:"
+cd ../SmbLnk2_EthIf
+git pull
+
+echo -e "\n\nSmbLnk2_Fee:"
+cd ../SmbLnk2_Fee
+git pull
+
+echo -e "\n\nSmbLnk2_FrIf:"
+cd ../SmbLnk2_FrIf
+git pull
+
+echo -e "\n\nSmbLnk2_FrNm:"
+cd ../SmbLnk2_FrNm
+git pull
+
+echo -e "\n\nSmbLnk2_FrTp:"
+cd ../SmbLnk2_FrTp
+git pull
+
+echo -e "\n\nSmbLnk2_LinIf:"
+cd ../SmbLnk2_LinIf
+git pull
+
+echo -e "\n\nSmbLnk2_LinTp:"
+cd ../SmbLnk2_LinTp
+git pull
+
+echo -e "\n\nSmbLnk2_MemIf:"
+cd ../SmbLnk2_MemIf
+git pull
+
+echo -e "\n\nSmbLnk2_WdgIf:"
+cd ../SmbLnk2_WdgIf
+git pull
+
+echo -e "\n\nSmbLnk2_Adc:"
+cd ../SmbLnk2_Adc
+git pull
+
+echo -e "\n\nSmbLnk2_Can:"
+cd ../SmbLnk2_Can
+git pull
+
+echo -e "\n\nSmbLnk2_Cry:"
+cd ../SmbLnk2_Cry
+git pull
+
+echo -e "\n\nSmbLnk2_Dio:"
+cd ../SmbLnk2_Dio
+git pull
+
+echo -e "\n\nSmbLnk2_Eep:"
+cd ../SmbLnk2_Eep
+git pull
+
+echo -e "\n\nSmbLnk2_Eth:"
+cd ../SmbLnk2_Eth
+git pull
+
+echo -e "\n\nSmbLnk2_Fls:"
+cd ../SmbLnk2_Fls
+git pull
+
+echo -e "\n\nSmbLnk2_Fr:"
+cd ../SmbLnk2_Fr
+git pull
+
+echo -e "\n\nSmbLnk2_Gpt:"
+cd ../SmbLnk2_Gpt
+git pull
+
+echo -e "\n\nSmbLnk2_Icu:"
+cd ../SmbLnk2_Icu
+git pull
+
+echo -e "\n\nSmbLnk2_IpduM:"
+cd ../SmbLnk2_IpduM
+git pull
+
+echo -e "\n\nSmbLnk2_J1939Tp:"
+cd ../SmbLnk2_J1939Tp
+git pull
+
+echo -e "\n\nSmbLnk2_Lin:"
+cd ../SmbLnk2_Lin
+git pull
+
+echo -e "\n\nSmbLnk2_Mcu:"
+cd ../SmbLnk2_Mcu
+git pull
+
+echo -e "\n\nSmbLnk2_Ocu:"
+cd ../SmbLnk2_Ocu
+git pull
+
+echo -e "\n\nSmbLnk2_Port:"
+cd ../SmbLnk2_Port
+git pull
+
+echo -e "\n\nSmbLnk2_Pwm:"
+cd ../SmbLnk2_Pwm
+git pull
+
+echo -e "\n\nSmbLnk2_Spi:"
+cd ../SmbLnk2_Spi
+git pull
+
+echo -e "\n\nSmbLnk2_Wdg:"
+cd ../SmbLnk2_Wdg
+git pull
+
+echo -e "\n\nSmbLnk2_BswM:"
+cd ../SmbLnk2_BswM
+git pull
+
+echo -e "\n\nSmbLnk2_Com:"
+cd ../SmbLnk2_Com
+git pull
+
+echo -e "\n\nSmbLnk2_ComM:"
+cd ../SmbLnk2_ComM
+git pull
+
+echo -e "\n\nSmbLnk2_Csm:"
+cd ../SmbLnk2_Csm
+git pull
+
+echo -e "\n\nSmbLnk2_Dcm:"
+cd ../SmbLnk2_Dcm
+git pull
+
+echo -e "\n\nSmbLnk2_Dem:"
+cd ../SmbLnk2_Dem
+git pull
+
+echo -e "\n\nSmbLnk2_Det:"
+cd ../SmbLnk2_Det
+git pull
+
+echo -e "\n\nSmbLnk2_Dlt:"
+cd ../SmbLnk2_Dlt
+git pull
+
+echo -e "\n\nSmbLnk2_EcuM:"
+cd ../SmbLnk2_EcuM
+git pull
+
+echo -e "\n\nSmbLnk2_FiM:"
+cd ../SmbLnk2_FiM
+git pull
+
+echo -e "\n\nSmbLnk2_Nm:"
+cd ../SmbLnk2_Nm
+git pull
+
+echo -e "\n\nSmbLnk2_NvM:"
+cd ../SmbLnk2_NvM
+git pull
+
+echo -e "\n\nSmbLnk2_Os:"
+cd ../SmbLnk2_Os
+git pull
+
+echo -e "\n\nSmbLnk2_PduR:"
+cd ../SmbLnk2_PduR
+git pull
+
+echo -e "\n\nSmbLnk2_SchM:"
+cd ../SmbLnk2_SchM
+git pull
+
+echo -e "\n\nSmbLnk2_SecOC:"
+cd ../SmbLnk2_SecOC
+git pull
+
+echo -e "\n\nSmbLnk2_SokFm:"
+cd ../SmbLnk2_SokFm
+git pull
+
+echo -e "\n\nSmbLnk2_StartUp:"
+cd ../SmbLnk2_StartUp
+git pull
+
+echo -e "\n\nSmbLnk2_StbM:"
+cd ../SmbLnk2_StbM
+git pull
+
+echo -e "\n\nSmbLnk2_Vkms:"
+cd ../SmbLnk2_Vkms
+git pull
+
+echo -e "\n\nSmbLnk2_WdgM:"
+cd ../SmbLnk2_WdgM
+git pull
+
+echo -e "\n\nSmbLnk2_Compiler:"
+cd ../SmbLnk2_Compiler
+git pull
+
+echo -e "\n\nSmbLnk2_Rte:"
+cd ../SmbLnk2_Rte
+git pull
+
+echo -e "\n\nSmbLnk2_SwcApplFoc:"
+cd ../SmbLnk2_SwcApplFoc
+git pull
+
+echo -e "\n\nSmbLnk2_SwcServiceEcuM:"
+cd ../SmbLnk2_SwcServiceEcuM
+git pull
+
+echo -e "\n\nSmbLnk2_SwcServiceOs:"
+cd ../SmbLnk2_SwcServiceOs
+git pull
+
+echo -e "\n\nSmbLnk2_common:"
+cd ../SmbLnk2_common
+git pull
+
