65811d3bd8524dd17e258ec3aa6130f6058e31b0
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2022-08-03 17:37:51 +0200

    Update: AUTOSAR basic software common integration files

---
+++ BldcFocSensorless_LATEST/ComStackTypes.hpp
@@ -0,0 +1,84 @@
+#pragma once
+/******************************************************************************/
+/* File   : ComStackTypes.hpp                                                 */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #INCLUDES                                                                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define MaxIdPdu                                                             256 //TBD: To be generated in CfgComStack.hpp
+#define MaxLengthPdu                                                         256 //TBD: To be generated in CfgComStack.hpp
+
+/******************************************************************************/
+/* MACROS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* TYPEDEFS                                                                   */
+/******************************************************************************/
+typedef uint8 TypeIdPdu;         // 0-MaxIdPdu     (accordingly extend to uint16)
+typedef uint8 TypeLengthPdu;     // 0-MaxLengthPdu (accordingly extend to uint16)
+typedef uint8 TypeHandlePNC;     // to store partial network cluster id
+typedef uint8 TypeHandleNetwork; // to store communication channel id
+typedef uint8 TypeIdConfigIcom;  // Icom configuration id, 0 = default. More than one config set possible
+
+typedef enum{
+      eParameterTpStMin = 0      // Seperation time
+   ,  eParameterTpBS             // Block size
+   ,  eParameterTpBC             // Bandwidth control, used in flexray
+}TypeParameterTp;
+
+typedef enum{
+      eReturnReqBufOK = 0
+   ,  eReturnReqBufNotOK
+   ,  eReturnReqBufBusy
+   ,  eReturnReqBufOfl
+}TypeReturnReqBuf;
+
+typedef enum{
+      eStateDataTpConf = 0
+   ,  eStateDataTpRetry
+   ,  eStateDataTpConfPending
+}TypeStateDataTp;
+
+typedef enum{
+      eErrorSwitchIcomOK = 0
+   ,  eErrorSwitchIcomFailed
+}TypeErrorSwitchIcom;
+
+typedef struct{
+   uint8*        ptrDataSdu;
+   uint8*        ptrDataMeta;
+   TypeLengthPdu LengthPdu;
+}TypeInfoPdu;
+
+typedef struct{
+   TypeStateDataTp StatueDataTp;
+   TypeLengthPdu   LengthPdu;
+}TypeInfoRetry;
+
+/******************************************************************************/
+/* CONSTS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* PARAMS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+
+/******************************************************************************/
+/* FUNCTIONS                                                                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
