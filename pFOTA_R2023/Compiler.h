ef140812b33fa188675db4de4ec28211e27337d9
Raajnaag HULIYAPURADA MATA <nagaraja.hm.0001@gmail.com>
2023-07-12 16:24:35 +0200

    Initial commit

--- pFOTA_R2023/Compiler.h
+++ pFOTA_R2023/Compiler.h
@@ -43,19 +43,19 @@
 #define NULL_PTR                                                     ((void *)0)
 #endif
 
-#define INLINE                                                          __inline
-#define LOCAL_INLINE                                               static inline
-#define FUNC(rettype, memclass)       __attribute__((section(memclass))) rettype
-#define FUNC_P2CONST(rettype, ptrclass, memclass)                 const rettype*
-#define FUNC_P2VAR(rettype, ptrclass, memclass)                         rettype*
-#define P2VAR(ptrtype, memclass, ptrclass)                              ptrtype*
-#define P2CONST(ptrtype, memclass, ptrclass)                      const ptrtype*
-#define CONSTP2VAR(ptrtype, memclass, ptrclass)                   ptrtype *const
-#define CONSTP2CONST(ptrtype, memclass, ptrclass)           const ptrtype *const
-#define P2FUNC(rettype, ptrclass, fctname)                    rettype (*fctname)
-#define CONSTP2FUNC(rettype, ptrclass, fctname)         rettype (*const fctname)
-#define CONST(type, memclass)                                         const type
-#define VAR(vartype, memclass)                                           vartype
+#define INLINE                                    __inline
+#define LOCAL_INLINE                              static inline
+#define FUNC(rettype, memclass)                   __attribute__((section(memclass))) rettype
+#define FUNC_P2CONST(rettype, ptrclass, memclass) const rettype*
+#define FUNC_P2VAR(rettype, ptrclass, memclass)   rettype*
+#define P2VAR(ptrtype, memclass, ptrclass)        ptrtype*
+#define P2CONST(ptrtype, memclass, ptrclass)      const ptrtype*
+#define CONSTP2VAR(ptrtype, memclass, ptrclass)   ptrtype *const
+#define CONSTP2CONST(ptrtype, memclass, ptrclass) const ptrtype *const
+#define P2FUNC(rettype, ptrclass, fctname)        rettype (*fctname)
+#define CONSTP2FUNC(rettype, ptrclass, fctname)   rettype (*const fctname)
+#define CONST(type, memclass)                     __attribute__((section(memclass))) const type
+#define VAR(vartype, memclass)                    __attribute__((section(memclass))) vartype
 
 /******************************************************************************/
 /* MACROS                                                                     */
